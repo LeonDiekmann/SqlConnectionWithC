7e6294c4061ee07f01129ad9c76d0026e2547f5d
Leon Diekmann <Leon_Diekmann@web.de>
2018-09-10 17:35:54 +0200

    added stored procedure and select for address table

--- SqlConnectionWithC/Program.cs
+++ SqlConnectionWithC/Program.cs
@@ -21,6 +21,10 @@ namespace SqlConnectionWithC
                     Console.Clear();
                     Employee();
                     break;
+                case "address":
+                    Console.Clear();
+                    address();
+                    break;
             }
         }
 
@@ -33,11 +37,12 @@ namespace SqlConnectionWithC
             {
                 Console.Clear();
                 conn.Open();
-                SqlCommand cmd = new SqlCommand("SELECT FirstName,LastName,Birthday,Job,PostCode,City,Street,HouseNumber,Country FROM [dbo].[viEmployee]", conn);
+                SqlCommand cmd = new SqlCommand("SELECT Id, FirstName,LastName,Birthday,Job,PostCode,City,Street,HouseNumber,Country FROM [dbo].[viEmployee]", conn);
                 SqlDataReader reader = cmd.ExecuteReader();
                 while (reader.Read())
                 {
-                    Console.WriteLine("Vorname: {0},\nNachname: {1},\nGeburtstag: {2},\nJob: {3},\nPostleitzahl: {4},\nWohnort: {5},\nStraße: {6},\nHausnummer: {7},\nLand: {8}\n", reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetValue(4), reader.GetString(5), reader.GetString(6), reader.GetValue(7), reader.GetString(8));
+                    Console.WriteLine("ID: {0} \nVorname: {1}\nNachname: {2}\nGeburtstag: {3}\nJob: {4}\nPostleitzahl: {5}\nWohnort: {6}\nStraße: {7}\nHausnummer: {8}\nLand: {9}\n", 
+                        reader.GetValue(0),reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4), reader.GetValue(5), reader.GetString(6), reader.GetString(7), reader.GetValue(8), reader.GetString(9));
                 }
                 reader.Close();
                 conn.Close();
@@ -49,7 +54,6 @@ namespace SqlConnectionWithC
             }
             else
             {
-                
                 conn.Open();
                 SqlCommand cmd = new SqlCommand("dbo.spEmployee", conn);
                 cmd.CommandType = CommandType.StoredProcedure;
@@ -76,9 +80,66 @@ namespace SqlConnectionWithC
                 //Console.WriteLine("Insert a Department");
                 //cmd.Parameters.Add(new SqlParameter("@DepartmentId", Convert.ToInt32(Console.ReadLine())));
                 //Console.Clear();
+                cmd.ExecuteNonQuery();
+                conn.Close();
 
+                Console.WriteLine("Stored procedure executed!");
+                if (Debugger.IsAttached)
+                {
+                    Console.ReadLine();
+                }
 
+            }
+        }
 
+        public static void address()
+        {
+            Console.WriteLine("What do you want to do?");
+            SqlConnection conn = new SqlConnection("Server=tappqa;Database=Training-LD-CompanyDB;Integrated Security=true");
+
+            if (Console.ReadLine() == "select")
+            {
+                Console.Clear();
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT Id, PostCode, City, Street, HouseNumber, Country FROM [dbo].[viAddress]", conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine("Id: {0} \nPostleitzahl: {1} \nStadt: {2} \nStraße: {3} \nHausnummer: {4} \nLand: {5}\n", reader.GetValue(0), reader.GetValue(1), reader.GetString(2), reader.GetString(3), reader.GetValue(4), reader.GetString(5));
+                }
+                reader.Close();
+                conn.Close();
+
+                if (Debugger.IsAttached)
+                {
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("dbo.spAddress", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                Console.Clear();
+                Console.WriteLine("Insert an Id");
+                cmd.Parameters.Add(new SqlParameter("@Id", Console.ReadLine()));
+                Console.Clear();
+                Console.WriteLine("Insert a post code");
+                cmd.Parameters.Add(new SqlParameter("@PostCode", Console.ReadLine()));
+                Console.Clear();
+                Console.WriteLine("Insert a city");
+                cmd.Parameters.Add(new SqlParameter("@City", Console.ReadLine()));
+                Console.Clear();
+                Console.WriteLine("Insert a street");
+                cmd.Parameters.Add(new SqlParameter("@Street", Console.ReadLine()));
+                Console.Clear();
+                Console.WriteLine("Insert a housenumber");
+                cmd.Parameters.Add(new SqlParameter("@HouseNumber", Console.ReadLine()));
+                Console.Clear();
+                Console.WriteLine("Insert a country");
+                cmd.Parameters.Add(new SqlParameter("@Country", Console.ReadLine()));
+                Console.Clear();
                 cmd.ExecuteNonQuery();
                 conn.Close();
 
